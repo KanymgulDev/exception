3772b50d3d6ac7fe0b409501b9b7012d0c80aa44
kanymgul05032003 <abdubaitova05@mail.ru>
2023-04-29 07:34:20 +0600

    Initial commit

---
+++ src/Javacoor.java
@@ -0,0 +1,17 @@
+public class Javacoor {
+     String name;
+     int age;
+     public void setName(String name1){
+          name1=name;
+
+     }
+     public String getName(){
+          return name;
+     }
+     public void setAge (int num){
+          num=age;
+     }
+     public int setAge(){
+          return age;
+     }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Javacoor objekt=new Javacoor() 
+
+    }
+}
